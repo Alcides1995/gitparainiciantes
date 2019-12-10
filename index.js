3fc422ef78903149976d39fe50ffadc2a7b30859
alcides.limachi <alcideslimachi5@gmail.com>
2019-12-10 08:16:01 -0300

    nossa primeiro API

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+ res.status(200).send({ message: 'Olá mundo esse é meu repositorio clonado.' });
+})
+
+app.listen(4001, () => {
+  console.log('Api rodando na porta 4001');
+})
